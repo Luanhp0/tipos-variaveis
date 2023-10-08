8c99c6998f9e120234050e137f9ace766551780e
Luanhp0 <luanpereirahp0@gmail.com>
2023-10-07 23:57:06 -0300

    Atualizacao

--- src/TiposVariaveis.java
+++ src/TiposVariaveis.java
@@ -1,5 +1,31 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+        // classe String que representa texto na aplicacao 
+
+        String meuNome = "Luan Pereira";
+
+        double salarioMinimo = 2.50;
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333;
+        long cpf = 2145865845L;
+        float pi = 3.14f;
+
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+        
+
     }
 }
